a4f9320e1595763a2b895a77c730e8982b50e74e
cag2050 <cag2050@163.com>
2018-03-31 12:04:07 +0800

    m

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,7 @@ module.exports = function override(config, env) {
     // config = injectBabelPlugin(['import', {libraryName: 'antd', libraryDirectory: 'es', style: 'css'}], config);
     config = injectBabelPlugin(['import', {libraryName: 'antd', libraryDirectory: 'es', style: true}], config);
     config = rewireLess.withLoaderOptions({
-        modifyVars: {"@primary-color": "#1DA57A"},
+        // modifyVars: {"@primary-color": "#1DA57A"},
     })(config, env);
     return config;
 };
