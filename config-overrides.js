e12befd37023b107b019d1a9fbc7df4a88d63487
cag2050 <cag2050@163.com>
2018-03-31 11:59:42 +0800

    m

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,12 @@
 const {injectBabelPlugin} = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
     // do stuff with the webpack config...
-    config = injectBabelPlugin(['import', {libraryName: 'antd', libraryDirectory: 'es', style: 'css'}], config);
+    // config = injectBabelPlugin(['import', {libraryName: 'antd', libraryDirectory: 'es', style: 'css'}], config);
+    config = injectBabelPlugin(['import', {libraryName: 'antd', libraryDirectory: 'es', style: true}], config);
+    config = rewireLess.withLoaderOptions({
+        modifyVars: {"@primary-color": "#1DA57A"},
+    })(config, env);
     return config;
 };
