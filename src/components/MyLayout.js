0aa49c1525d6b70dc9f891e9849e4b771af4f99d
cag2050 <cag2050@163.com>
2018-04-01 21:13:56 +0800

    m

--- src/components/MyLayout.js
+++ src/components/MyLayout.js
@@ -34,12 +34,12 @@ class MyLayout extends React.Component {
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1">
               <Icon type="user"/>
-              <span>nav 1</span>
+              <span>button</span>
               <Link to='/'></Link>
             </Menu.Item>
             <Menu.Item key="2">
               <Icon type="video-camera"/>
-              <span>nav 2</span>
+              <span>input</span>
               <Link to='/aaa'></Link>
             </Menu.Item>
             <Menu.Item key="3">
